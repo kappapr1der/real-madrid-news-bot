75d98d502a447a6e353b18b952b20b77e21bd860
kappapr1der <th32062@gmail.com>
2026-06-19 14:26:06 +0300

    Refresh live Russian RSS sources

--- sources_ru.py
+++ sources_ru.py
@@ -1,22 +1,8 @@
-REAL_MADRID_RU_SOURCES = [
-    {"url": "https://www.sports.ru/rss/team/real-madrid.xml", "label": "Sports.ru – Реал Мадрид"},
-]
+REAL_MADRID_RU_SOURCES = []
 
 GENERAL_RU_FOOTBALL_SOURCES = [
-    {"url": "https://rsport.ria.ru/export/rss2/archive/index.xml", "label": "РИА Новости Спорт"},
-    {"url": "https://www.sport-express.ru/services/materials/news/", "label": "Спорт-Экспресс"},
-    {"url": "https://www.championat.com/xml/rss_news.xml", "label": "Чемпионат"},
-    {"url": "https://www.sovsport.ru/rss/all.xml", "label": "Советский спорт"},
-    {"url": "https://www.gazeta.ru/export/soccer.xml", "label": "Газета.ру"},
-    {"url": "https://sportrbc.ru/rss/", "label": "РБК Спорт"},
     {"url": "https://www.sports.ru/rss/main.xml", "label": "Sports.ru"},
-    {"url": "https://www.eurosport.ru/rss.xml", "label": "Eurosport Russia"},
-    {"url": "https://news.sportbox.ru/Vidy_sporta/Futbol/rss", "label": "Sportbox"},
-    {"url": "https://www.championat.com/football/_ucl.xml", "label": "Чемпионат – ЛЧ"},
-    {"url": "https://www.championat.com/football/_euroleague.xml", "label": "Чемпионат – ЛЕ"},
-    {"url": "https://www.livesport.ru/rss/all.xml", "label": "Livesport"},
-    {"url": "https://www.sportsdaily.ru/rss.xml", "label": "SportsDaily"},
-    {"url": "https://www.metaratings.ru/rss/all.xml", "label": "MetaRatings"},
+    {"url": "https://www.championat.com/rss/news/football/", "label": "Чемпионат – Футбол"},
 ]
 
 SOURCES_RU = REAL_MADRID_RU_SOURCES + GENERAL_RU_FOOTBALL_SOURCES
